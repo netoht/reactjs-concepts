a1783627edd86d20fa10c06cf409d1e867d32761
Waldinar Neto <waldinar.neto@loft.com.br>
2021-10-06 01:59:33 -0300

    feat: added immutable code

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -25,25 +25,22 @@ export function TaskList() {
       isComplete: false,
     };
 
-    setTasks([...tasks, newTask]);
+    setTasks((oldState) => [...oldState, newTask]);
     setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === id) {
-          task.isComplete = !task.isComplete;
-        }
-        return task;
-      })
+    const newTasks = tasks.map((task) =>
+      task.id === id ? { ...task, isComplete: !task.isComplete } : task
     );
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    setTasks(tasks.filter((task) => task.id !== id));
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
